72763d9726279a7db798bfb34ecda1d841009cbd
apexcel <dev.apexcel@gmail.com>
2021-01-27 21:32:43 +0900

    Add visualization of loading

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 import App from './App.js';
-new App(document.getElementById('app'));
+
+const app = document.getElementById('app');
+const docElem = document.documentElement;
+const loading = document.createElement('div');
+
+let isLoading = true;
+(() => {
+    app.className = 'hide'
+    loading.className = 'loading'
+    docElem.append(loading);
+
+    isLoading = false;
+    start()
+})();
+
+function start() {
+    if (!isLoading) {
+        console.log(isLoading)
+        app.classList.remove('hide');
+        docElem.removeChild(loading);
+        new App(app);
+    }
+}
